75a4245d22653e9d83748239fb85bdd7f297ce02
Peter Spychalsky <23511567+theonlycakes@users.noreply.github.com>
2019-12-23 18:30:17 -0500

    Added the ability to create a folder

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,18 +2,21 @@ package com.company;
 import java.io.File;
 
 
+
 public class Main {
 
     public static void main(String[] args) {
         //makes the GUI
         GUI q = new GUI();
-        //Add folder generation for specific name if does not exist
-
-
-        //Reads the files from a folder that is in the same location as the jar file
+        //gets location of jar file
         String jarLoc = Main.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+
+        //Makes a folder to add the data to which it can read from
+        File dir = new File(jarLoc + "Scouting Data");
+        dir.mkdir();
+        //Reads the files from a folder that is in the same location as the jar file
         //Need to change folder name for release
-        File file = new File(jarLoc + "/Test files/");
+        File file = new File(jarLoc + "/Scouting Data/");
         File[] files = file.listFiles();
         for(File f: files){
             System.out.println(f.getName());
