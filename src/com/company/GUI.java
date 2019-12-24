45273744dd6045fc5014ec4494096582110f1ca2
Peter Spychalsky <23511567+theonlycakes@users.noreply.github.com>
2019-12-23 20:46:56 -0500

    Added some test data and it now closes

--- src/com/company/GUI.java
+++ src/com/company/GUI.java
@@ -1,14 +1,22 @@
 package com.company;
 import java.awt.*;
+import java.awt.event.*;
 
 public class GUI extends Frame{
 GUI() {
     Frame f = new Frame();
-    f.setSize(300,300);
-    Button b=new Button("click me");
+    f.setSize(300, 300);
+    Button b = new Button("click me");
     f.add(b);
+
+
     f.setVisible(true);
-}
-
-
+    //allows the program to be closed
+    f.addWindowListener(new WindowAdapter() {
+        @Override
+        public void windowClosing(WindowEvent e) {
+            System.exit(0);
+        }
+    });
+    }
 }
